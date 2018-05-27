324506b345c8108aae9aac28ac7d62cd5e9410d8
GaoHeming111 <651812464@qq.com>
2018-05-27 10:56:30 +0800

    initial commit

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="search">
+    <input style="text" placeholder="输入城市名或拼音" class="search-input">
+</div>
+</template>
+
+<script>
+export default {
+    name:"CitySearch"
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+.search
+    height .72rem
+    background $bgColor
+    padding 0 .1rem
+    .search-input
+        box-sizing border-box
+        padding 0 .1rem
+        height .62rem
+        line-height .62rem
+        width 100%
+        text-align center
+        border-radius .06rem
+        color #666
+        border none
+</style>
