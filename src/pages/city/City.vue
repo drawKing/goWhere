6bd8ed2e8f7ef099189554939a2e4fac0fd4866f
GaoHeming111 <651812464@qq.com>
2018-05-27 13:45:49 +0800

    initial commit

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,17 +2,23 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
+        <city-alphabet></city-alphabet>
+        <city-list></city-list>
     </div>
 </template>
 
 <script>
 import CityHeader from './components/Header.vue'
 import CitySearch from './components/Search.vue'
+import CityAlphabet from './components/Alphabet.vue'
+import CityList from './components/List.vue'
 export default {
     name:"City",
     components :{
         CityHeader,
-        CitySearch
+        CitySearch,
+        CityAlphabet,
+        CityList
     }
 }
 </script>
